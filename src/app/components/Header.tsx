3a0561a36bc56437fb42e1b7cf0a94ef82b0ac89
Ben <kwalijkje@live.com>
2024-06-22 13:16:55 +0200

    Initial setup. Added header and login component skeletons

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,44 @@
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+} from "@nextui-org/navbar";
+import { Button } from "@nextui-org/react";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <Navbar position="static">
+      <NavbarBrand>
+        <p className="font-bold text-inherit">Badger Apps</p>
+      </NavbarBrand>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem isActive>
+          <Link href="#" aria-current="page">
+            Home
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Watchlist
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+};
+export default Header;
