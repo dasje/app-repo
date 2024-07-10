71b61dadcab4c8d495cd3947f6919b60afdb3618
Ben <kwalijkje@live.com>
2024-07-10 10:43:16 +0200

    User watchlist fetching logic moved to client component

--- src/app/apps/watchlist/components/AvailableWatchlists.tsx
+++ src/app/apps/watchlist/components/AvailableWatchlists.tsx
@@ -1,17 +1,57 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-"use server";
+"use client";
 import { UserType, getUser } from "@/app/lib/handlers/getUser";
 import { WatchlistContentTable } from "@/app/database/types";
 import Watchlist from "./Watchlist";
 import { watchlistResponse } from "../page";
 import { watch } from "fs";
+import { useEffect, useState } from "react";
 
 interface AvailableWatchlists {
   user: UserType;
-  watchlists: watchlistResponse;
 }
-const AvailableWatchlists = ({ user, watchlists }: AvailableWatchlists) => {
-  console.log("HERE ", watchlists);
+
+export type watchlistResponse = {
+  message: {
+    id: string;
+    watchlist_id: string;
+    user_id: string;
+    name: string;
+    created_at: string;
+    created_by: string;
+  }[];
+};
+
+const AvailableWatchlists = ({ user }: AvailableWatchlists) => {
+  const [watchlists, setWatchlists] = useState<watchlistResponse>();
+
+  useEffect(() => {
+    const getWatchlists = async () => {
+      var userLists: watchlistResponse;
+      user &&
+        (await fetch(
+          process.env.NEXT_PUBLIC_URL + "/api/watchlist/user-watchlists",
+          {
+            method: "POST",
+            body: JSON.stringify({ userEmail: user.email }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        ).then(async (res) => {
+          if (res.status !== 200) {
+            console.log("Error caught");
+            userLists = { message: [] };
+          } else {
+            userLists = await res.json();
+            console.log("Success fetching user lists", userLists);
+            setWatchlists(userLists);
+          }
+        }));
+    };
+    getWatchlists();
+  }, [user]);
+
   return (
     <>
       {watchlists &&
--- src/app/apps/watchlist/page.tsx
+++ src/app/apps/watchlist/page.tsx
@@ -4,53 +4,8 @@ import { getUser } from "@/app/lib/handlers/getUser";
 import AvailableWatchlists from "./components/AvailableWatchlists";
 import AddWatchlist from "./components/AddWatchlist";
 
-export type watchlistResponse = {
-  message: {
-    id: string;
-    watchlist_id: string;
-    user_id: string;
-    name: string;
-    created_at: string;
-    created_by: string;
-  }[];
-};
-
 export default async function Page() {
   const user = await getUser();
-  var userLists: watchlistResponse;
-  user &&
-    (await fetch(
-      process.env.NEXT_PUBLIC_URL + "/api/watchlist/user-watchlists",
-      {
-        method: "POST",
-        body: JSON.stringify({ userEmail: user.email }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    ).then(async (res) => {
-      if (res.status !== 200) {
-        console.log("Error caught");
-        userLists = { message: [] };
-      } else {
-        userLists = await res.json();
-        console.log("Success fetching user lists", userLists);
-      }
-    }));
-  const shows = [
-    {
-      id: "1",
-      media_name: "Star Trek 123432",
-      watchlist_list_id: "001",
-      watched: 1,
-    },
-    {
-      id: "2",
-      media_name: "Star Trek 87543798",
-      watchlist_list_id: "001",
-      watched: 0,
-    },
-  ];
 
   return (
     <>
@@ -64,7 +19,7 @@ export default async function Page() {
       ) : (
         <div>
           <AddWatchlist user={user} />
-          <AvailableWatchlists user={user} watchlists={userLists} />
+          <AvailableWatchlists user={user} />
         </div>
       )}
     </>
