d7c14124f36c3266a1e26c88dbad602931cef8a2
Ben <kwalijkje@live.com>
2024-07-14 23:57:07 +0200

    Create sendInviteEmail.ts

---
+++ src/app/lib/handlers/sendInviteEmail.ts
@@ -0,0 +1,48 @@
+import { NextRequest, NextResponse } from "next/server";
+import { Resend } from "resend";
+// import * as z from "zod";
+import BadgerAppsInviteUserEmail from "../templates/BadgerAppsInviteUserEmail";
+
+interface sendEmailInterface {
+  emailTo: string;
+  subject: string;
+  username?: string;
+  //   userImage?: string;
+  invitedByUsername?: string;
+  invitedByEmail?: string;
+  appName?: string;
+  appImage?: string;
+  inviteLink?: string;
+  //   inviteFromIp?: string;
+  //   inviteFromLocation?: string;
+}
+export const sendEmail = async ({
+  emailTo,
+  subject,
+  //   username,
+  invitedByUsername,
+  invitedByEmail,
+  appName,
+  appImage,
+  inviteLink,
+}: sendEmailInterface) => {
+  const resend = new Resend(process.env.REACT_APP_RESEND_API_KEY);
+
+  await resend.emails
+    .send({
+      from: process.env.REACT_APP_INFO_EMAIL,
+      to: emailTo,
+      subject: subject,
+      react: BadgerAppsInviteUserEmail({
+        // username,
+        invitedByUsername,
+        invitedByEmail,
+        appName,
+        appImage,
+        inviteLink,
+      }),
+    })
+    .then((res) => {
+      return "success";
+    });
+};
