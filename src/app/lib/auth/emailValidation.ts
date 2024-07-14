3ba151dc65db0430205dad0d166ebac40cafcd37
Ben <kwalijkje@live.com>
2024-07-14 23:51:52 +0200

    Adjusted regex for email and password validations

--- src/app/lib/auth/emailValidation.ts
+++ src/app/lib/auth/emailValidation.ts
@@ -1,4 +1,5 @@
 export function emailValidation(email: string) {
-  const expression: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+  const expression: RegExp =
+    /(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/;
   return expression.test(email);
 }
--- src/app/lib/auth/passwordValidation.ts
+++ src/app/lib/auth/passwordValidation.ts
@@ -1,5 +1,5 @@
 export function passwordValidation(password: string) {
   const expression: RegExp =
-    /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/;
+    /(?=.*\d.*)(?=.*[a-zA-Z].*)(?=.*[!#\$%&\?].*).{8,}/;
   return expression.test(password);
 }
