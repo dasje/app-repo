ddfed07a1c61452eb83201ae5577e38209ab7a2c
Ben <kwalijkje@live.com>
2024-09-06 12:54:37 +0200

    API route added for fetching password reset request. Only returns ok status for password requests within 3 hour window

---
+++ src/app/api/register/forgot-password/[request_code]/route.ts
@@ -0,0 +1,38 @@
+import { findValidPasswordReset } from "@/app/database/repositories/PasswordResetRepository";
+
+type Params = {
+  request_code: string;
+};
+
+export async function GET(request: Request, context: { params: Params }) {
+  const requestCode = context.params.request_code;
+  console.log("Fetching request code: ", requestCode);
+  var items;
+  try {
+    items = await findValidPasswordReset(requestCode);
+  } catch (error: any) {
+    console.log("Error in find-password-request route.");
+    if (error.code === "ER_TOO_MANY_USER_CONNECTIONS") {
+      console.log("Too many connections error.");
+      return Response.json(
+        {
+          message: error.message,
+        },
+        { status: 503 }
+      );
+    }
+    console.log(error);
+    return Response.json(
+      {
+        message: error.message,
+      },
+      { status: 500 }
+    );
+  }
+  if (!items) {
+    console.log("No valid result found in find-password-request route.");
+    return Response.json({ status: 204 });
+  }
+  console.log("Success in find-password-request route", items);
+  return Response.json({ message: items }, { status: 200 });
+}
